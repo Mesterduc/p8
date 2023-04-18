c8652c5e1b071df60f40bd4227d68beccf53cd6b
signegj <115215831+signegj@users.noreply.github.com>
2023-04-18 10:23:21 +0200

    start of popup

---
+++ P8/Assets/Scripts/PopUp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PopUp : MonoBehaviour
+
+{
+    // [SerializeField]
+    public Button button; 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // private void OnMouseDown() 
+    // {
+    //     if (Input.GetMouseButtonDown)
+    //     {
+    //         button.SetActive(true);
+    //     }
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
