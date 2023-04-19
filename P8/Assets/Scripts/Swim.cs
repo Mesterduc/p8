2fbdcb7c4ee8d344c34ab0e679908d9b21f9bf45
hmql <115213628+hmql@users.noreply.github.com>
2023-04-19 13:20:31 +0200

    Test
    swim script

---
+++ P8/Assets/Scripts/Swim.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swim : MonoBehaviour
+{
+    public float speed;
+    public float range;
+    public float maxDistance;
+
+    Vector2 waypoint; 
+    
+    void Start()
+    {
+        setNewDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,waypoint, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position,waypoint) < range)
+        {
+            setNewDestination(); 
+        }
+    }
+    
+    void setNewDestination()
+    {
+        waypoint = new Vector2(Random.Range(-maxDistance, maxDistance),Random.Range (-maxDistance, maxDistance));
+    }
+}
