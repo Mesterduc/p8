0d9739101f398cdfb9b87b68894d8d7854361ea2
hmql <115213628+hmql@users.noreply.github.com>
2023-04-21 11:52:36 +0200

    Movement
    Animal script

---
+++ P8/Assets/Scripts/Animal/Animal.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Animal : MonoBehaviour
+{
+    private string name;
+    private string spicies;
+    private string type; 
+
+}
