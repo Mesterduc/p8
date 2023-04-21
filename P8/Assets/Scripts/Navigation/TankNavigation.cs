bd89e4f38e520b3b2cd58b036fc1fb61c0296329
Hong Duc Cai <hongduccai@gmail.com>
2023-04-21 20:59:42 +0200

    Update TankNavigation.cs

--- P8/Assets/Scripts/Navigation/TankNavigation.cs
+++ P8/Assets/Scripts/Navigation/TankNavigation.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Navigation;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TankNavigation : MonoBehaviour
-{
+public class TankNavigation : MonoBehaviour {
+    [SerializeField] private Button _menu;
+    [SerializeField] private Button _achievements;
     // Start is called before the first frame update
     void Start()
     {
+        _achievements.onClick.AddListener( () => ScenesManager.Instance.LoadSceneName(ScenesManager.Scene.AchievementsScene));
+        _menu.onClick.AddListener( () => ScenesManager.Instance.LoadSceneName(ScenesManager.Scene.Menu));
         
     }
 
