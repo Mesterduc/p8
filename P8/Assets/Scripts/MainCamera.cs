5cef335b4df1b7a2096941e3950651ba2b94713e
magnusedb <115003394+magnusedb@users.noreply.github.com>
2023-04-25 10:56:42 +0200

    mineændringer

--- P8/Assets/Scripts/MainCamera.cs
+++ P8/Assets/Scripts/MainCamera.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DataPersistence;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainCamera : MonoBehaviour
 {
@@ -7,6 +13,8 @@ public class MainCamera : MonoBehaviour
     public float zoomSpeed = 0.1f; // Hvor hurtigt den zoomer
     public float minZoom = 1f; // Min zoom level
     public float maxZoom = 5f; // Max zoom level
+    [SerializeField] public GameObject modal;
+    private Boolean isHidden = true;
 
 
     void Update()
@@ -28,5 +36,18 @@ public class MainCamera : MonoBehaviour
         float newZoom = Camera.main.orthographicSize - zoomInput * zoomSpeed;
         newZoom = Mathf.Clamp(newZoom, minZoom, maxZoom);
         Camera.main.orthographicSize = newZoom;
+        
+
+        if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+        // Cast a ray straight down.
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+
+        // If it hits something...
+        if (hit.collider == null)
+        {
+            modal.SetActive(isHidden);
+            isHidden = !isHidden;
+        }
+   }
     }
 }
