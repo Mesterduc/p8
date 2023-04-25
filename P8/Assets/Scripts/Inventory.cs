2f78ff8cc458aefc73049735b684de14f4c60162
hmql <115213628+hmql@users.noreply.github.com>
2023-04-25 10:50:29 +0200

    Inventory
    List / Button

--- P8/Assets/Scripts/Inventory.cs
+++ P8/Assets/Scripts/Inventory.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
+    public Button InventoryButton;
     public List<Fish> availableFish = new List<Fish>();
 
     public void AddFish(Fish fish)
@@ -13,7 +15,18 @@ public class Inventory : MonoBehaviour
     public void RemoveFish(Fish fish)
     {
         availableFish.Remove(fish);
+
+        void Start()
+        {
+            for (int i = 0; i < availableFish.Count; i++)
+            {
+                Fish fish = availableFish[i];
+                Button InventoryButton = Instantiate(InventoryButton, transform);
+
+            }
+
+        }
+
+
     }
-
-
 }
