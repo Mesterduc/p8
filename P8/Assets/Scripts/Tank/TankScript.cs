6f57d382804a39d02791ea0e7ead2b4adeed3ce7
Hong Duc Cai <hongduccai@gmail.com>
2023-04-26 12:12:21 +0200

    slet

--- P8/Assets/Scripts/Tank/TankScript.cs
+++ P8/Assets/Scripts/Tank/TankScript.cs
@@ -43,6 +43,7 @@ namespace Tank
                     move.animal = animal;
             }
         }
+        // TODO: pop up
         // TODO: Script
         // TODO: Husk position
         // TODO: Custom movement script 
