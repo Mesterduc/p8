17221befffb18a6ec124228ee998e356e1103008
Hong Duc Cai <hongduccai@gmail.com>
2023-04-26 10:23:08 +0200

    move classes to model folder

---
+++ P8/Assets/Scripts/Models/Fish.cs
@@ -0,0 +1,11 @@
+namespace Models {
+    // public class Fish : Animal
+    // {
+    //     public Fish(string name, string animated, bool isDisplayed, AnimalSize animalSize) : base(name, animated, isDisplayed, animalSize)
+    //     {
+    //         
+    //     }
+    // }
+}
+
+
---
+++ P8/Assets/Scripts/Models/Shrimp.cs
@@ -0,0 +1,31 @@
+using Animals;
+
+namespace Models {
+    // public class Shrimp : Animal
+    // {
+    //     public Shrimp(string name, string species, string type, float speed, string animated, AnimalSize fishSize, string realLifeImage, bool isDisplayed)
+    //         : base(name, species, type, animated, fishSize, realLifeImage, isDisplayed)
+    //     {
+    //
+    //     }
+    //
+    //     public override void Move()
+    //     {
+    //         //make it wiggle its tail
+    //         //throw new System.NotImplementedException();
+    //     }
+    //
+    //
+    //     // Start is called before the first frame update
+    //     void Start()
+    //     {
+    //     
+    //     }
+    //
+    //     // Update is called once per frame
+    //     void Update()
+    //     {
+    //     
+    //     }
+    // }
+}
