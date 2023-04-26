18ce0f4843eabd38b97f845b754304abe6531b11
hmql <115213628+hmql@users.noreply.github.com>
2023-04-26 13:35:45 +0200

    Inventory
    Add fish

--- P8/Assets/Scripts/Inventory.cs
+++ P8/Assets/Scripts/Inventory.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Inventory : MonoBehaviour
 {
@@ -9,17 +10,29 @@ public class Inventory : MonoBehaviour
     public GameObject FishPrefab;
     public List<Fish> availableFish = new List<Fish>();
     Fish Salmon = new Fish("test", "Salmon", "water animal", 1, "lol", AnimalSize.large, "lol", true);
-    Fish Herring = new Fish("test", "Salmon", "water animal", 1, "lol", AnimalSize.large, "lol", true);
+
+    Fish Herring = new Fish("test2", "Salmon", "water animal", 1, "lol", AnimalSize.large, "lol", true);
+
+    Fish Trout = new Fish("test3", "Salmon", "water animal", 1, "lol", AnimalSize.large, "lol", true);
+
     void Start()
     {
         AddFish(Salmon);
+        AddFish(Herring);
+        AddFish(Trout);
     }
     public void AddFish(Fish fish)
     {
         availableFish.Add(fish);
-        GameObject newFish = Instantiate(FishPrefab, transform.position, Quaternion.identity);
-        newFish.transform.SetParent(InventoryPanel, false);
-        newFish.transform.localPosition = Vector2.zero;
+        for (int i = 0; i < availableFish.Count; i++)
+        {
+            GameObject newFish = Instantiate(FishPrefab, transform.position, Quaternion.identity);
+            newFish.transform.SetParent(InventoryPanel, false);
+            newFish.transform.localPosition = new Vector3(i * 150, 0, 0);
+            TextMeshProUGUI[] fishInfo = newFish.GetComponentsInChildren<TextMeshProUGUI>();
+            fishInfo[0].text = availableFish[i].name;
+        }
+
 
 
     }
