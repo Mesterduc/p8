559ea751aefd7ef46be9bdb7c15efeceeccfb108
magnusedb <115003394+magnusedb@users.noreply.github.com>
2023-04-26 14:54:15 +0200

    johnson
    asd

--- P8/Assets/Scripts/magnus/johnson/MenuScript.cs
+++ P8/Assets/Scripts/magnus/johnson/MenuScript.cs
@@ -13,9 +13,11 @@ public class MenuScript : MonoBehaviour, IDataPersistence
     public List<Destination> destinations = new List<Destination>();
     public TMP_Text textcomponent;
 
-        [SerializeField] private GameObject destinationPrefab; 
-        [SerializeField] private Transform destinationList;
-        private string destinationID;
+    [SerializeField] private GameObject objectToSpawn; 
+    [SerializeField] private Transform destinationList;
+    private string destinationID;
+
+    public Vector3 spawnPosition;
 
     private void Awake()
         {
@@ -23,27 +25,35 @@ public class MenuScript : MonoBehaviour, IDataPersistence
 
     void Start()
     {
-       for (int i = 0; i < destinations.Count; i++)
-            {
+    //    for (int i = 0; i < destinations.Count; i++)
+    //         {
 
-                GameObject newDestination = Instantiate(destinationPrefab, destinationList); 
-                // Image
-                newDestination.transform.GetComponent<SpriteRenderer>().sprite =
-                    Resources.Load<Sprite>("DestinationIcon/" + destinations[i].type.name);
-                // Nested components: destination information
-                // Transform newDestinationInfo = newDestination.transform.GetChild(1).transform;
-                // newDestinationInfo.GetChild(0).GetComponent<TMP_Text>().text = destinations[i].name;
-                // newDestinationInfo.GetChild(1).GetComponent<TMP_Text>().text = destinations[i].information;
-                // newDestinationInfo.GetChild(2).GetComponent<TMP_Text>().text = destinations[i].type.name; 
-            }
+    //             GameObject objectToSpawn = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity); 
+    //             Image
+    //             objectToSpawn.transform.GetComponent<SpriteRenderer>().sprite =
+    //                 Resources.Load<Sprite>("DestinationIcon/" + destinations[i].type.name);
+    //             Nested components: destination information
+    //             Transform newDestinationInfo = newDestination.transform.GetChild(1).transform;
+    //             newDestinationInfo.GetChild(0).GetComponent<TMP_Text>().text = destinations[i].name;
+    //             newDestinationInfo.GetChild(1).GetComponent<TMP_Text>().text = destinations[i].information;
+    //             newDestinationInfo.GetChild(2).GetComponent<TMP_Text>().text = destinations[i].type.name; 
+    //         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+          if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn(); // Call the Spawn() method when the Space key is pressed
+        }
     }
 
+     void Spawn()
+    {
+        // Instantiate the objectToSpawn at the spawnPosition with no rotation
+        Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+    }
 
  public void ReadStringInput(string id)
         {
