d8fce691c12b3ba31cf69957bdf171ae00131217
magnusedb <115003394+magnusedb@users.noreply.github.com>
2023-04-26 11:53:20 +0200

    Tilføjet destinations i gamedata

--- P8/Assets/Scripts/magnus/johnson/MenuScript.cs
+++ P8/Assets/Scripts/magnus/johnson/MenuScript.cs
@@ -10,8 +10,7 @@ using System;
 [System.Serializable]
 public class MenuScript : MonoBehaviour, IDataPersistence
 {
-    private List<Friend> Friends;
-    public List<Destination> strande = new List<Destination>();
+    public List<Destination> destinations = new List<Destination>();
     public TMP_Text textcomponent;
 
     private void Awake()
@@ -20,16 +19,9 @@ public class MenuScript : MonoBehaviour, IDataPersistence
 
     void Start()
     {
-        Activity fang_krabber = new Activity("Krabbejagt", "Lav et net og gå i gang");
-        FishTrivia krabbe = new FishTrivia("Krabbe", "krabbebillede", fang_krabber, "Kød", "Almindelig", "Krabben er fandeme over det hele man");
-        Biome strand = new Biome("Stranden", fang_krabber, krabbe);
-        Destination Eriksen = new Destination("Eriksen", "Ballehage er et super sted at være om sommeren", strand);
-        Debug.Log(strande[0].name);
-
-
-        if(strande[0].name != null)
+        if(destinations[0].name != null)
             {
-                textcomponent.text = strande[0].name;
+                textcomponent.text = destinations[0].name;
             }
             else
             {
@@ -40,16 +32,17 @@ public class MenuScript : MonoBehaviour, IDataPersistence
     // Update is called once per frame
     void Update()
     {
+        
     }
 
     public void LoadData(GameData data)
     {
-        strande = data.strande;
+        destinations = data.destinations;
     }
 
     public void SaveData(GameData data)
     {
          // throw new System.NotImplementedException();
-        data.strande = this.strande;
+        data.destinations = this.destinations;
     }
 }
