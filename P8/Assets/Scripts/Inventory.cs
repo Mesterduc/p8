3bb54f0b466c874bdda2af43b6ec1dc8320e5ac1
hmql <115213628+hmql@users.noreply.github.com>
2023-04-26 11:31:09 +0200

    Inventory
    ..

--- P8/Assets/Scripts/Inventory.cs
+++ P8/Assets/Scripts/Inventory.cs
@@ -5,28 +5,27 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    public Button InventoryButton;
+    public Transform InventoryPanel;
+    public GameObject FishPrefab;
     public List<Fish> availableFish = new List<Fish>();
-
+    Fish Salmon = new Fish("test", "Salmon", "water animal", 1, "lol", AnimalSize.large, "lol", true);
+    Fish Herring = new Fish("test", "Salmon", "water animal", 1, "lol", AnimalSize.large, "lol", true);
+    void Start()
+    {
+        AddFish(Salmon);
+    }
     public void AddFish(Fish fish)
     {
         availableFish.Add(fish);
+        GameObject newFish = Instantiate(FishPrefab, transform.position, Quaternion.identity);
+        newFish.transform.SetParent(InventoryPanel, false);
+        newFish.transform.localPosition = Vector2.zero;
+
+
     }
     public void RemoveFish(Fish fish)
     {
         availableFish.Remove(fish);
-
-        void Start()
-        {
-            for (int i = 0; i < availableFish.Count; i++)
-            {
-                Fish fish = availableFish[i];
-                Button InventoryButton = Instantiate(InventoryButton, transform);
-
-            }
-
-        }
-
-
     }
+
 }
