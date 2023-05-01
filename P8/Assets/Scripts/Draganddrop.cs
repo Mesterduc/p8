52d8a19d9f62d6d2c34669fc2fe1441402fb6bbb
Hong Duc Cai <hongduccai@gmail.com>
2023-05-01 20:52:36 +0200

    delete fish on drag end

--- P8/Assets/Scripts/Draganddrop.cs
+++ P8/Assets/Scripts/Draganddrop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,11 @@ using UnityEngine.EventSystems;
 public class Draganddrop : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDragHandler, IDropHandler
 {
     Transform parentAfterDrag;
+    private Draganddrop dragedItem;
+
+    private void Start() {
+        
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -22,15 +28,16 @@ public class Draganddrop : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDr
 
     public void OnDrop(PointerEventData eventData)
     {
-        GameObject dropped = eventData.pointerDrag;
-        Draganddrop draggableItem = dropped.GetComponent<Draganddrop>();
-        Debug.Log(draggableItem.name);
-        draggableItem.parentAfterDrag = transform;
+        // GameObject dropped = eventData.pointerDrag;
+        // dragedItem = dropped.GetComponent<Draganddrop>();
+        // Destroy(dragedItem);
+        // dragedItem.parentAfterDrag = transform;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("end");
+        Destroy(gameObject);
         transform.SetParent(parentAfterDrag);
     }
 }
