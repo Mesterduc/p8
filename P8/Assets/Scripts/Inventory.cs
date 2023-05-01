a3c45ad54ecd78198fb41b54630a9c339eca0bf2
hmql <115213628+hmql@users.noreply.github.com>
2023-05-01 11:48:39 +0200

    Inventory
    .....

--- P8/Assets/Scripts/Inventory.cs
+++ P8/Assets/Scripts/Inventory.cs
@@ -11,40 +11,31 @@ public class Inventory : MonoBehaviour
     public Transform inventoryPanel;
     public GameObject animalPrefab;
     public List<Animal> availableAnimals = new List<Animal>();
+
     public GameObject scrollContent;
+
 
     void Start()
     {
-        // Instantiate some animals to add to the inventory
         Animal salmon = new Animal(1, "Salmon", "Salmon_Animated", "swim", true, AnimalSize.medium, new Movement(5f, 3f, 50f, Vector2.zero), new FishTrivia("Salmon", "description", new Activity("Swimming", "guide"), "habitat", "diet", "range"));
-        AddAnimal(salmon);
-        Animal Trout = new Animal(1, "Trout", "Salmon_Animated", "swim", true, AnimalSize.medium, new Movement(5f, 3f, 50f, Vector2.zero), new FishTrivia("Salmon", "description", new Activity("Swimming", "guide"), "habitat", "diet", "range"));
-        AddAnimal(Trout);
-        Animal Herring = new Animal(1, "Trout", "Salmon_Animated", "swim", true, AnimalSize.medium, new Movement(5f, 3f, 50f, Vector2.zero), new FishTrivia("Salmon", "description", new Activity("Swimming", "guide"), "habitat", "diet", "range"));
-        AddAnimal(Herring);
-        Animal fish = new Animal(1, "Trout", "Salmon_Animated", "swim", true, AnimalSize.medium, new Movement(5f, 3f, 50f, Vector2.zero), new FishTrivia("Salmon", "description", new Activity("Swimming", "guide"), "habitat", "diet", "range"));
-        AddAnimal(fish);
-        Animal fisk = new Animal(1, "Trout", "Salmon_Animated", "swim", true, AnimalSize.medium, new Movement(5f, 3f, 50f, Vector2.zero), new FishTrivia("Salmon", "description", new Activity("Swimming", "guide"), "habitat", "diet", "range"));
-        AddAnimal(fisk);
-        Animal animal = new Animal(1, "Trout", "Salmon_Animated", "swim", true, AnimalSize.medium, new Movement(5f, 3f, 50f, Vector2.zero), new FishTrivia("Salmon", "description", new Activity("Swimming", "guide"), "habitat", "diet", "range"));
-        AddAnimal(animal);
+        availableAnimals.Add(salmon);
+        Animal Herring = new Animal(1, "Herring", "Salmon_Animated", "swim", true, AnimalSize.medium, new Movement(5f, 3f, 50f, Vector2.zero), new FishTrivia("Salmon", "description", new Activity("Swimming", "guide"), "habitat", "diet", "range"));
+        availableAnimals.Add(Herring);
 
-
-    }
-
-    public void AddAnimal(Animal animal)
-    {
-        availableAnimals.Add(animal);
         for (int i = 0; i < availableAnimals.Count; i++)
         {
             GameObject newAnimal = Instantiate(animalPrefab, scrollContent.transform);
             newAnimal.transform.localPosition = new Vector3(i * 150, 0, 0);
             TextMeshProUGUI[] animalInfo = newAnimal.GetComponentsInChildren<TextMeshProUGUI>();
             animalInfo[0].text = availableAnimals[i].name;
-            availableAnimals[i].species = new FishTrivia(animal.name, "description", new Activity("Swimming", "Swimming guide"), "habitat", "diet", "range");
+            availableAnimals[i].species = new FishTrivia(availableAnimals[i].name, "description", new Activity("Swimming", "Swimming guide"), "habitat", "diet", "range");
         }
 
+
+
+
     }
+
 
     public void RemoveAnimal(Animal animal)
     {
