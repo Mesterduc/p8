766f4d2ff828b58817fbb1de4374583ec96c6b14
thorupp <115213314+thorupp@users.noreply.github.com>
2023-05-02 10:27:04 +0200

    ToggleGameinfo

---
+++ P8/Assets/Scripts/ExpandedInfo/InfoToggle.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoToggle : MonoBehaviour
+{
+    public GameObject info;
+
+    private static GameObject activeGameObject; // static variable to keep track of the currently active GameObject
+
+    private void Start()
+    {
+        info.SetActive(false);
+    }
+
+    public void OnButtonClick()
+    {
+        if (activeGameObject != null && activeGameObject != gameObject) // if there's another GameObject with an active info GameObject
+        {
+            activeGameObject.GetComponent<InfoToggle>().info.SetActive(false); // deactivate its info GameObject
+        }
+
+        activeGameObject = gameObject; // set this GameObject as the currently active one
+
+        info.SetActive(!info.activeSelf); // toggle the info GameObject of this GameObject
+    }
+}
