edc65e2c040e76aedc622fbd534e8fc1ace4cb9f
thorupp <115213314+thorupp@users.noreply.github.com>
2023-05-02 10:02:50 +0200

    FoodScript færdig(ish)
    Tror det er færdig, der mangler selve interaktionen mellem fisk

--- P8/Assets/Scripts/Tank/FoodScript.cs
+++ P8/Assets/Scripts/Tank/FoodScript.cs
@@ -5,38 +5,24 @@ using UnityEngine;
 public class FoodScript : MonoBehaviour
 {
     [SerializeField] GameObject[] foodPrefab;
-    [SerializeField] float secondSpawn = 0.5f; //Tid der går før mad spawner (SKAL SÆTTES PÅ KNAP)
     [SerializeField] float minX; //min x koordinat
     [SerializeField] float MaxX; //max x koordinat
-
-    public bool isEnabled = true;
-
-    void Start()
-    {
-        StartCoroutine(FoodSpawn()); //Kører en Coroutine som pauses af sidste linje yield, før den kører igen
-    }
-
-    IEnumerator FoodSpawn() //Spawner mad
-    {
-        while (true)
-        {
-            if (isEnabled = true)
-            {
-        
-                var xPos = Random.Range(minX, MaxX); //giver et tilfældigt x koordinat mellem de to givede kordinater (gives i unity)
-                var position = new Vector3(xPos, transform.position.y); //giver først xPos og derfter tager den det y pos some "Food" objectet har. Altså der hvor de bliver spawnet
-                GameObject foodObj = Instantiate(foodPrefab[Random.Range(0, foodPrefab.Length)], position, Quaternion.identity); //Geneerer et FoodObjekt fra en af de forskellige foodPrefabs på position(giver ovenfor) uden nogen nogen rotation
-                yield return new WaitForSeconds(secondSpawn); //holder pause i "secondSpawn" lang tid
-        
-            }
-        }   
     
-    }
+    float lifespan = 18f;
 
-    public void ToggleEnable()
+    int foodspawn = 3;
+
+    
+
+
+    public void FoodSpawn() //Spawner mad
     {
-        isEnabled = !isEnabled;
+        for (int i= 0; i<foodspawn; i++)
+        { 
+            var xPos = Random.Range(minX, MaxX); //giver et tilfældigt x koordinat mellem de to givede kordinater (gives i unity)
+            var position = new Vector3(xPos, transform.position.y); //giver først xPos og derfter tager den det y pos some "Food" objectet har. Altså der hvor de bliver spawnet
+            GameObject foodObj = Instantiate(foodPrefab[Random.Range(0, foodPrefab.Length)], position, Quaternion.Euler(Random.Range(0f, 0f), Random.Range(0f, 0f), Random.Range(0f, 360f))); //Geneerer et FoodObjekt fra en af de forskellige foodPrefabs på position(giver ovenfor) med mulighed for en 360 rotation på z aksen  (så det kun er 2d)   
+            Destroy(foodObj, lifespan);
+        }
     }
-
-
 }
