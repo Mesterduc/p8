bd5cc36a7876d0840766328f9961315d778184a1
Hong Duc Cai <hongduccai@gmail.com>
2023-05-04 19:25:11 +0200

    opdeling af gamedata

--- P8/Assets/Scripts/DataPersistence/GameData.cs
+++ P8/Assets/Scripts/DataPersistence/GameData.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace DataPersistence
 {
-    // Place data here 
+    // Place test data here 
     // [System.Serializable]
     public class GameData
     {
@@ -27,19 +27,14 @@ namespace DataPersistence
         }
 
         // loader program med standard data
-        private void PreLoad()
-        {
-            this.playerData = new PlayerData("Jeff");
-
-            friends.Add(new Friend("Johnson", 10, 4, "pedro"));
-            friends.Add(new Friend("Jax", 99, 99, "sifos"));
-            friends.Add(new Friend("Yuli", 490, 13, "yuli"));
-            friends.Add(new Friend("Sifos", 490, 13, "sifos"));
-
-            // Magnus' forsøg på at loade menu
-
-            Vector3 position = new Vector3(0, 0, 1);
-
+        private void PreLoad() {
+            måskeSlet();
+            createFish();
+            listOfSpices();
+            
+            // -- Destination information -- 
+            
+            // -------------------------------------------- Activity ---------------------------------------------------------------
             Activity fang_krabber = new Activity("Krabbejagt", "Find en tøjklemme og en snor. Put en blåmusling på tøjklemmen og sænk den ned i vandet. Så er der krabber!");
             Activity fang_fisk = new Activity("Fisketur", "Anskaf dig en fiskestang og se en video");
             Activity fang_insekter = new Activity("Insektjagt", "Insekter kan du fange sådan her:");
@@ -48,23 +43,21 @@ namespace DataPersistence
             List<Activity> hede_aktiviteter = new List<Activity>();
             hede_aktiviteter.Add(fang_edderkopper);
             hede_aktiviteter.Add(fang_insekter);
-            List<FishTrivia> hede_dyr = new List<FishTrivia>();
 
             List<Activity> strand_aktiviteter = new List<Activity>();
             strand_aktiviteter.Add(fang_fisk);
             strand_aktiviteter.Add(fang_krabber);
-            List<FishTrivia> strand_dyr = new List<FishTrivia>();
 
             List<Activity> havn_aktiviteter = new List<Activity>();
             havn_aktiviteter.Add(fang_fisk);
             havn_aktiviteter.Add(fang_krabber);
-            List<FishTrivia> havn_dyr = new List<FishTrivia>();
 
             List<Activity> skov_aktiviteter = new List<Activity>();
             skov_aktiviteter.Add(fang_edderkopper);
             skov_aktiviteter.Add(fang_insekter);
-            List<FishTrivia> skov_dyr = new List<FishTrivia>();
-
+            
+            //--------------------------------------------------- Trivia -------------------------------------------------------------------------------------
+            
             FishTrivia guldsmed = new FishTrivia("Guldsmed","Fish/Sild", "", fang_insekter, "Andre insekter", "Sjælden", "Guldsmeden flyver tit ved ferskvandssøer");
             FishTrivia hornbille = new FishTrivia("Hornbille", "Fish/Sild","",fang_insekter, "Bæ", "Almindelig", "Hornbiller har horn");
             FishTrivia husedderkop = new FishTrivia("Husedderkop","Fish/Sild", "",fang_edderkopper, "Fluer", "Sjælden", "Husedderkoppen kan blive KÆÆÆÆMPE stor");
@@ -80,35 +73,38 @@ namespace DataPersistence
             FishTrivia pighvar = new FishTrivia("Pighvar","Fish/Sild", "",fang_fisk, "Rejer, muslinger", "Sjælden", "Hans mener han er jordens bedste til at fange dem");
             FishTrivia sild = new FishTrivia("Sild", "Fish/Sild","",fang_fisk, "Alt", "Almindelig", "Du kan næsten ikke undgå at finde sild");
             FishTrivia torsk = new FishTrivia("Torsk", "Fish/Sild","",fang_fisk, "Alt", "Sjælden", "Engang var der mange torsk. Nu kan de være svære at finde");
-
+            
+            List<FishTrivia> hede_dyr = new List<FishTrivia>();
             hede_dyr.Add(hornbille);
             hede_dyr.Add(husedderkop);
             hede_dyr.Add(stinkbille);
             hede_dyr.Add(spyflue);
-
+            
+            List<FishTrivia> skov_dyr = new List<FishTrivia>();
             skov_dyr.Add(guldsmed);
             skov_dyr.Add(spyflue);
             skov_dyr.Add(snegl);
             skov_dyr.Add(tæge);
-
+            
+            List<FishTrivia> strand_dyr = new List<FishTrivia>();
             strand_dyr.Add(krabbe);
             strand_dyr.Add(ørred);
             strand_dyr.Add(hornfisk);
             strand_dyr.Add(skrubbe);
             strand_dyr.Add(pighvar);
-
+            
+            List<FishTrivia> havn_dyr = new List<FishTrivia>();
             havn_dyr.Add(torsk);
             havn_dyr.Add(sild);
             havn_dyr.Add(hornfisk);
-            
-            
 
-
+            // --------------------------------------- Biome --------------------------------------------------------------------
             Biome strand = new Biome("Strand", strand_aktiviteter, strand_dyr);
             Biome havn = new Biome("Havn", havn_aktiviteter, havn_dyr);
             Biome skov = new Biome("Skov", skov_aktiviteter, skov_dyr);
             Biome hede = new Biome("Hede", hede_aktiviteter, hede_dyr);
-
+            
+            // --------------------------------------- Destination --------------------------------------------------------------------
             Destination dall_hede = new Destination("Dall Hede", "Dall hede er et sted man kan slappe af om sommeren.", hede, new Vector3(-2.288f, 4.229f, 1f));
             Destination egholm = new Destination("Egholm", "En stor mark med en masse insekter", hede, new Vector3(-2.28f, 5.497f, 1f));
             Destination rold_skov = new Destination("Rold Skov", "Rold skov er fandeme ikke et sted du skal slappe for meget af", skov, new Vector3(-1.859f, 3.659f, 1f));
@@ -132,11 +128,20 @@ namespace DataPersistence
             destinations.Add(egholm);
             destinations.Add(lundby);
 
-            
+        }
 
-            // animalpictures.Add(krabbe);
-            // animalpictures.Add(laks);
-            // animalpictures.Add(fladfisk);
+        private void måskeSlet() {
+            this.playerData = new PlayerData("Jeff");
+
+            friends.Add(new Friend("Johnson", 10, 4, "pedro"));
+            friends.Add(new Friend("Jax", 99, 99, "sifos"));
+            friends.Add(new Friend("Yuli", 490, 13, "yuli"));
+            friends.Add(new Friend("Sifos", 490, 13, "sifos"));
+        }
+
+        private void createFish() {
+            Activity fang_krabber = new Activity("Krabbejagt", "Find en tøjklemme og en snor. Put en blåmusling på tøjklemmen og sænk den ned i vandet. Så er der krabber!");
+            FishTrivia krabbe = new FishTrivia("Krabbe","Fish/Sild","", fang_krabber, "Kød", "Almindelig", "Krabben er fandeme over det hele man");
             // Movement move = new Movement(150, 20, 400);
             Animal predo4 = new Animal(4, "Predo4", "Fish/Sild", "Fish/SildAnimator", AnimalSize.Small, krabbe);
             predo4.isDisplayed = false;
@@ -147,16 +152,17 @@ namespace DataPersistence
             animals.Add(new Animal(3,"Predo3", "Fish/Sild", "Fish/SildAnimator", AnimalSize.Small, krabbe));
             animals.Add(predo4);
             animals.Add(predo5);
-            
+        }
+
+        private void listOfSpices() {
+            Activity fang_krabber = new Activity("Krabbejagt", "Find en tøjklemme og en snor. Put en blåmusling på tøjklemmen og sænk den ned i vandet. Så er der krabber!");
+            Activity fang_fisk = new Activity("Fisketur", "Anskaf dig en fiskestang og se en video");
+            Activity fang_edderkopper = new Activity("Edderkoppe-Eventyr", "Find et edderkoppenet. Find en flue. Put en flue i nettet. WIN!");
             
             FishTrivia lundsneglen = new FishTrivia("Lundsneglen", "DyrIcon/snail", "Real/lundsnegl" , fang_krabber, "Kød", "Almindelig", "Lundsneglen er en mellemstor snegl med fladt, kugleformet hus og mørk skalåbning. Huset bliver mellem 12-22 mm i højde og 18-25 mm i brede, huset varierer i farve og tegning fra gullig til rødlig eller brunlig med eller uden mørke spiralbånd. >Kroppen er grålig");
-            
             FishTrivia havesnegl = new FishTrivia("Havesnegl", "DyrIcon/snail", "Real/havesnegl" ,fang_fisk, "Andre fisk", "Almindelig", "Havesneglens hus varierer meget i farven fra brunt til rødt og gult. Snailen bliver ca. 2 cm lang og huset bliver ca. 2 cm i diameter. Huset kan være ensfarvet, eller det kan have 5 mørke bånd, som snor sig rundt om sneglehuset.");
-            
             FishTrivia myre = new FishTrivia("Sorte havemyrer", "DyrIcon/myre", "Real/myre" ,fang_fisk, "Rejer, muslinger", "Almindelig", "Sorte arbejder havemyrer: 3,5-5 mm. lang. Farven er gråbrun til sortbrun med lysere ben, og følehorn med en matglinsende overflade. Tæt behåring med varierende længde. Skællet er lavt og bredt med parallelle sider og lige overkant.");
-           
             FishTrivia edderkop = new FishTrivia("Korsedderkop", "DyrIcon/spider", "Real/Korsedderkop" ,fang_edderkopper, "Insekter", "Almindelig", "Korsedderkoppen har oftest på oversiden et lyst mønster, der ligner et kors. Korset er sammensat af mindre hvide pletter, der dog hos store individer kan mangle eller være meget små, så korset er utydeligt. Bagkroppen er altid mere eller mindre trekantet, og spidser til bagtil. Forrest på bagkroppen ses som regel to små pukler, der forstærker den trekantede facon. Hannens kropsmål er ca. 8 mm, mens hunnen bliver op til ca. 17 mm, når den om efteråret er fuld af æg. I modsætning til de voksne dyr er ungerne gule med sorte aftegninger.");
-            
             FishTrivia kratløber = new FishTrivia("Kratløber", "DyrIcon/stinkbille", "Real/Kratløber" ,fang_edderkopper, "Insekter", "Almindelig", "Kratløber bliver 22-26 mm. i længde. En ret stor og noget kompakt løbebille, som på afstand udmærker sig ved en svagt brunlig eller blåviolet farve på dækvingerne. Kanten af pronotum og dækvingerne er ofte klart violette. Det bedste kendetegn er en generel violetfarvning kombineret med, at der på hver dækvinge er tre rækker af tydeligt runde fordybninger, ca. 10 i hver række.");
             
             // FishTrivia reje = new FishTrivia("Reje", "rejebillede", fang_rejer, "plankton", "Almindelig", "Du kan næsten ikke undgå at finde rejer");
@@ -166,9 +172,6 @@ namespace DataPersistence
             species.Add(myre);
             species.Add(edderkop);
             species.Add(kratløber);
-            // animalpictures.Add(reje);
-            // animalpictures.Add(myg);
-
         }
     }
 }
