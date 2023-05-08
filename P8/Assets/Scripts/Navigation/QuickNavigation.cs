866fe4a5db0605822947cc9fd77815cf6f0a83da
Hong Duc Cai <hongduccai@gmail.com>
2023-05-08 21:49:29 +0200

    Update QuickNavigation.cs

--- P8/Assets/Scripts/Navigation/QuickNavigation.cs
+++ P8/Assets/Scripts/Navigation/QuickNavigation.cs
@@ -8,17 +8,17 @@ public class QuickNavigation : MonoBehaviour
 {
      public void GoToMap()
     {
-        SceneManager.LoadSceneAsync("menu");
+        ScenesManager.Instance.LoadSceneName(ScenesManager.Scene.Menu);
     }
 
      public void GoToPlay()
     {
-        SceneManager.LoadSceneAsync("playtrip");
+        ScenesManager.Instance.LoadSceneName(ScenesManager.Scene.PlayTrip);
     }
 
      public void GoToGallery()
     {
-        SceneManager.LoadSceneAsync("gallery");
+        ScenesManager.Instance.LoadSceneName(ScenesManager.Scene.Gallery);
     }
      public void GoToCamera()
     {
@@ -26,15 +26,15 @@ public class QuickNavigation : MonoBehaviour
     }
      public void GoToTank()
     {
-        SceneManager.LoadSceneAsync("TankScene");
+        ScenesManager.Instance.LoadSceneName(ScenesManager.Scene.TankScene);
     }
      public void GoToChoose()
     {
-        SceneManager.LoadSceneAsync("ChooseTrip"); 
+        ScenesManager.Instance.LoadSceneName(ScenesManager.Scene.ChooseTrip);
     }
 
       public void GoToAnimal()
     {
-        SceneManager.LoadSceneAsync("AddAnimalsScene");
+        ScenesManager.Instance.LoadSceneName(ScenesManager.Scene.AddAnimalsScene);
     }
 }
