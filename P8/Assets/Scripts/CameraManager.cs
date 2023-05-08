eb707c56b86ab2a12bb69d35cf3512553f5373b0
Hong Duc Cai <hongduccai@gmail.com>
2023-05-08 21:51:12 +0200

    retake and save

--- P8/Assets/Scripts/CameraManager.cs
+++ P8/Assets/Scripts/CameraManager.cs
@@ -7,37 +7,48 @@ using UnityEngine.UI;
 public class CameraManager : MonoBehaviour {
     private bool camAvailable;
     private WebCamTexture webcam;
+    public RawImage background;
+    [SerializeField] private Button snapshot;
+    [SerializeField] private Button retakeImageButton;
+    [SerializeField] private Button SaveImageButton;
+    [SerializeField] private GameObject AcceptPanel;
 
     // private Texture defaultBackground;
-    public RawImage background;
-    public AspectRatioFitter fit;
+    // public AspectRatioFitter fit;
 
-    [SerializeField] private Button snapshot;
 
     // TODO: front or back-side camera
     void Awake() {
         // StartCoroutine: bruges til at stoppe programmet
         snapshot.onClick.AddListener(() => StartCoroutine(takeSnapShot()));
+        retakeImageButton.onClick.AddListener(RetakeImage);
+        SaveImageButton.onClick.AddListener(SaveImage);
     }
 
     IEnumerator Start() {
         yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-        if (!Application.HasUserAuthorization(UserAuthorization.WebCam)) {
-            Debug.Log("No auth");
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
+            webcam = new WebCamTexture(Screen.width, Screen.height);
+            webcam.Play();
+            background.texture = webcam;
         }
-        webcam = new WebCamTexture(Screen.width, Screen.height);
-        webcam.Play();
-        background.texture = webcam;
     }
 // IEnumerator for at kunne bruge StartCoroutine
      private IEnumerator takeSnapShot() {
         snapshot.gameObject.SetActive(false);
         yield return new WaitForEndOfFrame(); // venter til slutningen af et frame, hvor ui elementerne er fjernet før der tages et snapshot/screenshot
         ScreenCapture.CaptureScreenshot("hong.png");
-        snapshot.gameObject.SetActive(true);
+        AcceptPanel.gameObject.SetActive(true);
     }
-    
-    
+
+     public void RetakeImage() {
+         snapshot.gameObject.SetActive(true);
+         AcceptPanel.gameObject.SetActive(false);
+     }
+
+     public void SaveImage() {
+         Debug.Log("save");
+     }
     void Update() {
         
         // if (!camAvailable) return;
