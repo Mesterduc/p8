f22819081fff3fc1bc4fd568fdf8de00f87d3cf5
Hong Duc Cai <hongduccai@gmail.com>
2023-05-10 18:48:41 +0200

    name

---
+++ P8/Assets/Scripts/Map/DestinationClick.cs
@@ -0,0 +1,32 @@
+using DataStore;
+using UnityEngine;
+
+namespace Map {
+    public class DestinationClick : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Awake()
+        { 
+
+        }
+
+        void OnMouseDown()
+        {
+            GameObject menu = GameObject.Find("UI");
+            MenuScript script = menu.GetComponent<MenuScript>();
+
+            script.ShowModal(this.name, transform.position);
+
+            GameObject maincamera = GameObject.Find("MainCamera");
+            MainCamera camerascript = maincamera.GetComponent<MainCamera>();
+
+            camerascript.MoveToSelection(transform.position);
+
+            Hogsmeade.nextTrip = int.Parse(this.name);
+            Debug.Log(Hogsmeade.nextTrip);
+
+        }
+
+
+    }
+}
